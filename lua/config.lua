00d2faac10a8bc659d1fe471059c856e70e11509
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-08-07 08:02:35 +0800

    doc: add `MulticharSurroundOpts` type for better lsp suggestions

--- lua/config.lua
+++ lua/config.lua
@@ -1,6 +1,13 @@
 local M = {}
 
+---@class MulticharSurroundOpts
+---@field prompt_text string
+---An array of tuples; each tuple has two elements — the left delimiter and the right delimiter.
+---For example: `{ { "(", ")" }, { "[", "]" }, { "{", "}" }, { "<", ">" } }`.
+---@field matching_pairs [string, string][]
+
 -- default config
+---@type MulticharSurroundOpts
 M.config_table = {
   prompt_text = "Edit right pair:",
   matching_pairs = {
@@ -8,6 +15,7 @@ M.config_table = {
   },
 }
 
+---@param user_config_table MulticharSurroundOpts
 M.make_config = function(user_config_table)
   user_config_table = user_config_table or {}
   M.config_table = vim.tbl_deep_extend("force", M.config_table, user_config_table)
